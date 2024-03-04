b5824b12356a60bf0859a3d7858a7e8a04a81ecd
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2024-03-04 18:46:49 -0400

    Creando modelo vehiculoDetalles

---
+++ Randy_Fanbian_P2_Ap1/Models/VehiculoDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Randy_Fanbian_P2_Ap1.Models
+{
+    public class VehiculoDetalle
+    {
+
+        [Key]
+        public int VehiculoDetalleId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int VehiculoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int AccesorioId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public decimal ValorId { get; set; }
+
+    }
+}
