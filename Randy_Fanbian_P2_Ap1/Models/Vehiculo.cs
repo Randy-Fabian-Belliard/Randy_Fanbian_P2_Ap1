dac3f6653ad51f46106252524de401f2ea333e13
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2024-03-04 18:46:19 -0400

    Creando modelo vehiculos

---
+++ Randy_Fanbian_P2_Ap1/Models/Vehiculo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Randy_Fanbian_P2_Ap1.Models
+{
+    public class Vehiculo
+    {
+
+            [Key]
+            public int  VehiculoId { get; set; }
+        
+            [Required(ErrorMessage = "Este campo es requerido")]
+            public DateTime Fecha { get; set; } = DateTime.Today;
+        
+            [Required(ErrorMessage = "Este campo es requerido")]
+            public string? Descripcion { get; set; }
+        
+            [Required(ErrorMessage = "Este campo es requerido")]
+            [Range(1, int.MaxValue, ErrorMessage = "El campo debe ser mayor que cero")]
+            public decimal Precio { get; set; }
+
+            [ForeignKey("VehiculoId")]
+            public List<VehiculoDetalle> VehiculoDetalle { get; set; } = new List<VehiculoDetalle>();
+        
+    }
+}
+
+
+
+
