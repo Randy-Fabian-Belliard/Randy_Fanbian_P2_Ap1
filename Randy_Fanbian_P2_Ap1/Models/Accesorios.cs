69c78e80e23ea1484dd67c47983e56c95a732734
Randy <133064874+Randy-Fabian-Belliard@users.noreply.github.com>
2024-03-04 18:47:09 -0400

    Creando modelo accesorios

---
+++ Randy_Fanbian_P2_Ap1/Models/Accesorios.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Randy_Fanbian_P2_Ap1.Models
+{
+    public class Accesorios
+    {
+        [Key]
+        public int AccesorioId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+
+        [ForeignKey("AccesorioId")]
+        public List<VehiculoDetalle> VehiculoDetalle { get; set; } = new List<VehiculoDetalle>();
+    }
+}
